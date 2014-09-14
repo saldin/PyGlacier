48c726f98f6c66c99b81135503794eb7bbed39b8
saldin <zhaocn@live.cn>
2014-09-14 12:18:14 +0800

    add test file

--- test.py
+++ test.py
@@ -1,1 +1,15 @@
 __author__ = 'cnzhao'
+from Person_pb2 import Person
+p = Person()
+p.id = 3
+p.str = "111"
+
+str = p.SerializeToString()
+
+print(str)
+
+po = Person()
+po.ParseFromString(str)
+
+print(po.id)
+print(po.str)
